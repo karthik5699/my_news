f5ebf21569032fc45ad9782fc1abf88cc0d8a538
karthik <karthiksingh5699@gmail.com>
2017-12-29 23:00:58 +0530

    first working state

---
+++ js/main2.js
@@ -0,0 +1,74 @@
+var json_data;
+var marticles = [];
+var search;
+var searched_news_url;
+var marticles2 = [];
+
+function createCORSRequest(method,url){
+	var xhr = new XMLHttpRequest();
+	if("withCredentials" in xhr){
+		xhr.open(method,url,true);
+	}
+	else if(typeof XDomainRequest!="undefined"){
+		xhr = new XDomainRequest();
+		xhr.open(method,url);
+	}
+	else{
+		xhr = null;
+	}
+	return xhr;
+}
+function makeCorsRequest(){
+	var xhr = createCORSRequest('GET',"https://newsapi.org/v2/top-headlines?sources=bloomberg&apiKey=05a33d3d62c74c5884c50c024a3480cf");
+	xhr.onload = function(){
+		var data = xhr.responseText;
+		json_data = JSON.parse(data);
+		marticles = json_data.articles;
+		
+		for(var i=0;i<marticles.length;i++){
+			var article = marticles[i];
+
+			$('#text-holder').append("<div class='article_title col-md-12'>"+'<a href="'+article.url+'" class="article-link">'+ article.title+"</a>"+"<div>");
+			$('#text-holder').append('<img src="'+article.urlToImage+'" class="img-fluid col-md-6">');
+			$('#text-holder').append('<p class="col-md-12">'+article.description+'</p>');
+		};
+	}
+	xhr.onerror = function(){
+		console.log("error");
+	}
+	xhr.send();
+}
+
+makeCorsRequest();
+
+
+
+	
+
+
+function call(){
+	
+	search = $('#news-input').val();
+	searched_news_url ="https://newsapi.org/v2/top-headlines?sources=bloomberg&apiKey=05a33d3d62c74c5884c50c024a3480cf"+"&q="+search;
+	var searched_articles;
+	console.log(searched_news_url);
+	var xhr2 = createCORSRequest('GET',searched_news_url);
+	xhr2.onload = function(){
+		var data = xhr2.responseText;
+		json_data = JSON.parse(data);
+		marticles2 = json_data.articles;
+		console.log(marticles2);
+	}
+	xhr2.onerror = function(){
+		console.log("error");
+	}
+	xhr2.send();
+	return false;
+}
+
+$('#news-search-form').submit(call);
+
+
+
+
+
